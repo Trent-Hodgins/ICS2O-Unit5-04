ac05ef55362eedc9491a26cdbeee1583b949cb68
TrentHodgins <>
2021-06-02 16:46:33 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ document.getElementById('change-text').addEventListener('click', text)
 document.getElementById('hide').addEventListener('click', hide)
 document.getElementById('show').addEventListener('click', show)
 document.getElementById('background').addEventListener('click', background)
-//These five lines are all similar. They all tell the code to not do certain commands until a certain button is clicked.//
+// These five lines are all similar. They all tell the code to not do certain commands until a certain button is clicked.//
 
 function image () {
-  document.getElementById('picture').src = "images/Charles.jpg"
+  document.getElementById('picture').src = 'images/Charles.jpg'
 }
 
 function text () {
-  document.getElementById('text').innerHTML = "New Text"
+  document.getElementById('text').innerHTML = 'New Text'
 }
 
 function hide () {
-  document.getElementById('text').style.display = "none"
+  document.getElementById('text').style.display = 'none'
 }
 
 function show () {
-  document.getElementById('text').style.display = "block"
+  document.getElementById('text').style.display = 'block'
 }
 
 function background () {
-  document.body.style.backgroundColor = "red"
+  document.body.style.backgroundColor = 'red'
 }
-//These five functions are similar. Each one tells the buttons what to do//
+// These five functions are similar. Each one tells the buttons what to do//
